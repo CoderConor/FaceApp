b5af7eddd59706e09ed96db08ecc181fd526dc16
Conor <conormoloneycm@gmail.com>
2020-04-23 13:10:59 +0100

    set margins for image, console logged bounding box info from API only

--- faceapp/src/App.js
+++ faceapp/src/App.js
@@ -25,18 +25,19 @@ class App extends Component {
   }
   //event.target.value is how we get the value from the user input
   onInputChange = (event) => {
+    //setState is an asynchronous react function being called
   this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict(
-      Clarifai.COLOR_MODEL,
+      Clarifai.FACE_DETECT_MODEL,
       this.state.input
     )
       .then(
         function (response) {
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function (err) {// there was an error
         }
--- faceapp/src/AppComponents/FaceRecognition/FaceRecognition.js
+++ faceapp/src/AppComponents/FaceRecognition/FaceRecognition.js
@@ -3,8 +3,10 @@ import React from 'react';
 //imageUrl defined in app.js, passed as prop. Can enter any image url link
 const FaceRecognition = ({ imageUrl }) => {
     return (
-        <div className='center'>
-        <img alt='' src={imageUrl}/>
+        <div className='center ma'>
+        <div className='absolute mt2'>
+        <img alt='' src={imageUrl} width='500px' height='auto'/>
+        </div>
       </div>
     );
 }
