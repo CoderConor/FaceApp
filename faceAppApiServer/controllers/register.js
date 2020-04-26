be8f3248192d4db5fb95546fbe083d7e9884f4a2
Conor <conormoloneycm@gmail.com>
2020-04-26 01:21:57 +0100

    debugging

--- faceAppApiServer/controllers/register.js
+++ faceAppApiServer/controllers/register.js
@@ -5,6 +5,7 @@ const registerHandler = (req, res, db, bcrypt) => {
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission');
     }
+    console.log(email, password, name)
     const hash = bcrypt.hashSync(password);
     //trx to make code block a transaction, ie when you need to do more than 2 things at once
       db.transaction(trx => {
